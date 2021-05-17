7b2faa69bdd8d3abfa42698ef999831b35d84ee8
Eilidh MacNicol <eilidhmacnicol@gmail.com>
2021-05-17 18:38:33 +0100

    ENH: slicer improvements

--- nilearn/plotting/displays.py
+++ nilearn/plotting/displays.py
@@ -296,6 +296,171 @@ class CutAxes(BaseAxes):
                           ec=bg_color, fc=bg_color, alpha=1),
                 **kwargs)
 
+    def _add_markers(self, marker_coords, marker_color, marker_size, marker_labels, **kwargs):
+        """Plot markers
+
+        In the case of 'l' and 'r' directions (for hemispheric projections),
+        markers in the coordinate x == 0 are included in both hemispheres.
+
+        """
+        marker_coords_2d = _coords_3d_to_2d(marker_coords, self.direction)
+        xdata, ydata = marker_coords_2d.T
+
+        # Allow markers only in their respective hemisphere when appropriate
+        if self.direction in 'lr':
+            if not isinstance(marker_color, str) and \
+                    not isinstance(marker_color, np.ndarray):
+                marker_color = np.asarray(marker_color)
+            relevant_coords = []
+            xcoords, ycoords, zcoords = marker_coords.T
+            for cidx, xc in enumerate(xcoords):
+                if self.direction == 'r' and xc >= 0:
+                    relevant_coords.append(cidx)
+                elif self.direction == 'l' and xc <= 0:
+                    relevant_coords.append(cidx)
+            xdata = xdata[relevant_coords]
+            ydata = ydata[relevant_coords]
+            # if marker_color is string for example 'red' or 'blue', then
+            # we pass marker_color as it is to matplotlib scatter without
+            # making any selection in 'l' or 'r' color.
+            # More likely that user wants to display all nodes to be in
+            # same color.
+            if not isinstance(marker_color, str) and \
+                    len(marker_color) != 1:
+                marker_color = marker_color[relevant_coords]
+
+            if not isinstance(marker_size, numbers.Number):
+                marker_size = np.asarray(marker_size)[relevant_coords]
+
+        defaults = {'marker': 'o',
+                    'zorder': 1000}
+        for k, v in defaults.items():
+            kwargs.setdefault(k, v)
+
+        self.ax.scatter(xdata, ydata, s=marker_size,
+                        c=marker_color, **kwargs)
+
+        if marker_labels is not None:
+            for i, txt in enumerate(marker_labels):
+                self.ax.annotate(
+                    txt,
+                    (round(xdata[i], 1), round(ydata[i], 1)),
+                    fontsize='xx-small',
+                    color='white', zorder=3000)
+
+    def _add_lines(self, line_coords, line_values, cmap,
+                   vmin=None, vmax=None, directed=False, **kwargs):
+        """Plot lines
+
+        Parameters
+        ----------
+        line_coords : list of numpy arrays of shape (2, 3)
+            3d coordinates of lines start points and end points.
+
+        line_values : array_like
+            Values of the lines.
+
+        cmap : colormap
+            Colormap used to map line_values to a color.
+
+        vmin, vmax : float, optional
+            If not None, either or both of these values will be used to
+            as the minimum and maximum values to color lines. If None are
+            supplied the maximum absolute value within the given threshold
+            will be used as minimum (multiplied by -1) and maximum
+            coloring levels.
+
+        directed : boolean, optional
+            Add arrows instead of lines if set to True. Use this when plotting
+            directed graphs for example. Default=False.
+
+        kwargs : dict
+            Additional arguments to pass to matplotlib Line2D.
+
+        """
+        # colormap for colorbar
+        self.cmap = cmap
+        if vmin is None and vmax is None:
+            abs_line_values_max = np.abs(line_values).max()
+            vmin = -abs_line_values_max
+            vmax = abs_line_values_max
+        elif vmin is None:
+            if vmax > 0:
+                vmin = -vmax
+            else:
+                raise ValueError(
+                    "If vmax is set to a non-positive number "
+                    "then vmin needs to be specified"
+                )
+        elif vmax is None:
+            if vmin < 0:
+                vmax = -vmin
+            else:
+                raise ValueError(
+                    "If vmin is set to a non-negative number "
+                    "then vmax needs to be specified"
+                )
+        norm = colors.Normalize(vmin=vmin,
+                                vmax=vmax)
+        # normalization useful for colorbar
+        self.norm = norm
+        abs_norm = colors.Normalize(vmin=0,
+                                    vmax=vmax)
+        value_to_color = plt.cm.ScalarMappable(norm=norm, cmap=cmap).to_rgba
+
+        # Allow lines only in their respective hemisphere when appropriate
+        if self.direction in 'lr':
+            relevant_lines = []
+            for lidx, line in enumerate(line_coords):
+                if self.direction == 'r':
+                    if line[0, 0] >= 0 and line[1, 0] >= 0:
+                        relevant_lines.append(lidx)
+                elif self.direction == 'l':
+                    if line[0, 0] < 0 and line[1, 0] < 0:
+                        relevant_lines.append(lidx)
+            line_coords = np.array(line_coords)[relevant_lines]
+            line_values = line_values[relevant_lines]
+
+        for start_end_point_3d, line_value in zip(
+                line_coords, line_values):
+            start_end_point_2d = _coords_3d_to_2d(start_end_point_3d,
+                                                  self.direction)
+
+            color = value_to_color(line_value)
+            abs_line_value = abs(line_value)
+            linewidth = 1 + 2 * abs_norm(abs_line_value)
+            # Hacky way to put the strongest connections on top of the weakest
+            # note sign does not matter hence using 'abs'
+            zorder = 10 + 10 * abs_norm(abs_line_value)
+            this_kwargs = {'color': color, 'linewidth': linewidth,
+                           'zorder': zorder}
+            # kwargs should have priority over this_kwargs so that the
+            # user can override the default logic
+            this_kwargs.update(kwargs)
+            xdata, ydata = start_end_point_2d.T
+            # If directed is True, add an arrow
+            if directed:
+                dx = xdata[1] - xdata[0]
+                dy = ydata[1] - ydata[0]
+                # Hack to avoid empty arrows to crash with
+                # matplotlib versions older than 3.1
+                # This can be removed once support for
+                # matplotlib pre 3.1 has been dropped.
+                if dx == 0 and dy == 0:
+                    arrow = FancyArrow(xdata[0], ydata[0],
+                                       dx, dy)
+                else:
+                    arrow = FancyArrow(xdata[0], ydata[0],
+                                       dx, dy,
+                                       length_includes_head=True,
+                                       width=linewidth,
+                                       head_width=3*linewidth,
+                                       **this_kwargs)
+                self.ax.add_patch(arrow)
+            # Otherwise a line
+            else:
+                line = lines.Line2D(xdata, ydata, **this_kwargs)
+                self.ax.add_line(line)
 
 def _get_index_from_direction(direction):
     """Returns numerical index from direction
@@ -1081,6 +1246,183 @@ class BaseSlicer(object):
             display_ax.ax.scatter(xdata, ydata, s=marker_size,
                                   c=marker_color_, **kwargs)
 
+    def add_graph(self, adjacency_matrix, node_coords,
+                  node_color='auto', node_size=50,
+                  node_labels=None, edge_cmap=cm.bwr,
+                  edge_vmin=None, edge_vmax=None,
+                  edge_threshold=None,
+                  edge_kwargs=None, node_kwargs=None, colorbar=False,
+                  ):
+        """Plot undirected graph on each of the axes
+
+        Parameters
+        ----------
+        adjacency_matrix : numpy array of shape (n, n)
+            Represents the edges strengths of the graph.
+            The matrix can be symmetric which will result in
+            an undirected graph, or not symmetric which will
+            result in a directed graph.
+
+        node_coords : numpy array_like of shape (n, 3)
+            3d coordinates of the graph nodes in world space.
+
+        node_color : color or sequence of colors, optional
+            Color(s) of the nodes. Default='auto'.
+
+        node_size : scalar or array_like, optional
+            Size(s) of the nodes in points^2. Default=50.
+
+        edge_cmap : colormap, optional
+            Colormap used for representing the strength of the edges.
+            Default=cm.bwr.
+
+        edge_vmin, edge_vmax : float, optional
+            If not None, either or both of these values will be used to
+            as the minimum and maximum values to color edges. If None are
+            supplied the maximum absolute value within the given threshold
+            will be used as minimum (multiplied by -1) and maximum
+            coloring levels.
+
+        edge_threshold : str or number, optional
+            If it is a number only the edges with a value greater than
+            edge_threshold will be shown.
+            If it is a string it must finish with a percent sign,
+            e.g. "25.3%", and only the edges with a abs(value) above
+            the given percentile will be shown.
+
+        edge_kwargs : dict, optional
+            Will be passed as kwargs for each edge matlotlib Line2D.
+
+        node_kwargs : dict
+            Will be passed as kwargs to the plt.scatter call that plots all
+            the nodes in one go.
+
+        """
+        # set defaults
+        if edge_kwargs is None:
+            edge_kwargs = {}
+        if node_kwargs is None:
+            node_kwargs = {}
+        if isinstance(node_color, str) and node_color == 'auto':
+            nb_nodes = len(node_coords)
+            node_color = mpl_cm.Set2(np.linspace(0, 1, nb_nodes))
+        node_coords = np.asarray(node_coords)
+
+        # decompress input matrix if sparse
+        if sparse.issparse(adjacency_matrix):
+            adjacency_matrix = adjacency_matrix.toarray()
+
+        # make the lines below well-behaved
+        adjacency_matrix = np.nan_to_num(adjacency_matrix)
+
+        # safety checks
+        if 's' in node_kwargs:
+            raise ValueError("Please use 'node_size' and not 'node_kwargs' "
+                             "to specify node sizes")
+        if 'c' in node_kwargs:
+            raise ValueError("Please use 'node_color' and not 'node_kwargs' "
+                             "to specify node colors")
+
+        adjacency_matrix_shape = adjacency_matrix.shape
+        if (len(adjacency_matrix_shape) != 2 or
+                adjacency_matrix_shape[0] != adjacency_matrix_shape[1]):
+            raise ValueError(
+                "'adjacency_matrix' is supposed to have shape (n, n)."
+                ' Its shape was {0}'.format(adjacency_matrix_shape))
+
+        node_coords_shape = node_coords.shape
+        if len(node_coords_shape) != 2 or node_coords_shape[1] != 3:
+            message = (
+                "Invalid shape for 'node_coords'. You passed an "
+                "'adjacency_matrix' of shape {0} therefore "
+                "'node_coords' should be a array with shape ({0[0]}, 3) "
+                'while its shape was {1}').format(adjacency_matrix_shape,
+                                                  node_coords_shape)
+
+            raise ValueError(message)
+
+        if isinstance(node_color, (list, np.ndarray)) and len(node_color) != 1:
+            if len(node_color) != node_coords_shape[0]:
+                raise ValueError(
+                    "Mismatch between the number of nodes ({0}) "
+                    "and and the number of node colors ({1})."
+                    .format(node_coords_shape[0], len(node_color)))
+
+        if node_coords_shape[0] != adjacency_matrix_shape[0]:
+            raise ValueError(
+                "Shape mismatch between 'adjacency_matrix' "
+                "and 'node_coords'"
+                "'adjacency_matrix' shape is {0}, 'node_coords' shape is {1}"
+                .format(adjacency_matrix_shape, node_coords_shape))
+
+        # If the adjacency matrix is not symmetric, give a warning
+        symmetric = True
+        if not np.allclose(adjacency_matrix, adjacency_matrix.T, rtol=1e-3):
+            symmetric = False
+            warnings.warn(("'adjacency_matrix' is not symmetric. "
+                           "A directed graph will be plotted."))
+
+        # For a masked array, masked values are replaced with zeros
+        if hasattr(adjacency_matrix, 'mask'):
+            if not (adjacency_matrix.mask == adjacency_matrix.mask.T).all():
+                symmetric = False
+                warnings.warn(("'adjacency_matrix' was masked with "
+                               "a non symmetric mask. A directed "
+                               "graph will be plotted."))
+            adjacency_matrix = adjacency_matrix.filled(0)
+
+        if edge_threshold is not None:
+            if symmetric:
+                # Keep a percentile of edges with the highest absolute
+                # values, so only need to look at the covariance
+                # coefficients below the diagonal
+                lower_diagonal_indices = np.tril_indices_from(adjacency_matrix,
+                                                              k=-1)
+                lower_diagonal_values = adjacency_matrix[
+                    lower_diagonal_indices]
+                edge_threshold = _utils.param_validation.check_threshold(
+                    edge_threshold, np.abs(lower_diagonal_values),
+                    stats.scoreatpercentile, 'edge_threshold')
+            else:
+                edge_threshold = _utils.param_validation.check_threshold(
+                    edge_threshold, np.abs(adjacency_matrix.ravel()),
+                    stats.scoreatpercentile, 'edge_threshold')
+
+            adjacency_matrix = adjacency_matrix.copy()
+            threshold_mask = np.abs(adjacency_matrix) < edge_threshold
+            adjacency_matrix[threshold_mask] = 0
+
+        if symmetric:
+            lower_triangular_adjacency_matrix = np.tril(adjacency_matrix, k=-1)
+            non_zero_indices = lower_triangular_adjacency_matrix.nonzero()
+        else:
+            non_zero_indices = adjacency_matrix.nonzero()
+
+        line_coords = [node_coords[list(index)]
+                       for index in zip(*non_zero_indices)]
+
+        adjacency_matrix_values = adjacency_matrix[non_zero_indices]
+        for ax in self.axes.values():
+            ax._add_markers(
+                node_coords,
+                node_color,
+                node_size,
+                node_labels,
+                **node_kwargs)
+            if line_coords:
+                ax._add_lines(line_coords, adjacency_matrix_values, edge_cmap,
+                              vmin=edge_vmin, vmax=edge_vmax,
+                              directed=(not symmetric), **edge_kwargs)
+            # To obtain the brain left view, we simply invert the x axis
+            if ax.direction == 'l' and not (ax.ax.get_xlim()[0] > ax.ax.get_xlim()[1]):
+                ax.ax.invert_xaxis()
+
+        if colorbar:
+            self._colorbar = colorbar
+            self._show_colorbar(ax.cmap, ax.norm, threshold=edge_threshold)
+
+        plt.draw_if_interactive()
+
     def annotate(self, left_right=True, positions=True, scalebar=False,
                  size=12, scale_size=5.0, scale_units='cm', scale_loc=4,
                  decimals=0, **kwargs):
@@ -1825,7 +2167,6 @@ class BaseStackedSlicer(BaseSlicer):
 class XSlicer(BaseStackedSlicer):
     _direction = 'x'
     _default_figsize = [2.6, 2.3]
-
 
 class YSlicer(BaseStackedSlicer):
     _direction = 'y'
